f443871251c8890b0eaacd4412c96ecd120a5c5f
G. Wade Johnson <gwadej@anomaly.org>
2010-11-03 01:17:03 +0000

    Get talks updated.

--- src/talks/2010talks/1009Talk/examples/renphoto.pl
+++ src/talks/2010talks/1009Talk/examples/renphoto.pl
@@ -3,16 +3,19 @@
 use strict;
 use warnings;
 
+# Process one file at a time from the command line
 foreach my $file ( @ARGV ) {
     my @stat = stat( $file );  # Get file attributes
     my $stamp = $stat[9];  # Modification time
-    my $date_time = date_time( $stamp );
+    my $date_time = date_time( $stamp ); # Make formatting the date easier.
     my $newname = $file;
+
+    # The next two lines really do the rename
     $newname =~ s/DSCN(\d+)\.JPG$/Vacation-$date_time-$1-wade.jpg/;
     rename( $file, $newname );
 }
 
-
+# Take a Unix epoch time and generate a human readable string out of it.
 sub date_time {
     my ($stamp) = @_;
 
