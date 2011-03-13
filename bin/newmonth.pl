7d091d0ad31196c7dae738587b06596336b5b913
G. Wade Johnson <gwadej@anomaly.org>
2011-03-13 05:16:16 +0000

    More functionality.

--- bin/newmonth.pl
+++ bin/newmonth.pl
@@ -8,6 +8,10 @@ use File::Path qw(mkpath);
 use autodie;
 use Template;
 use IO::Prompter;
+use JSON::XS;
+use XML::Atom::SimpleFeed;
+use File::Slurp;
+use POSIX qw(strftime);
 
 my @months = ( '', 'January', 'February', 'March', 'April', 'May', 'June',
     'July', 'August', 'September', 'October', 'November', 'December' );
@@ -22,9 +26,12 @@ $year = prompt( -integer => sub { 2010 <= $_ && $_ <= $year }, -def => $year, "Y
 my $author = prompt( "Author:" );
 my $title  = prompt( "Title:" );
 my $attendees = prompt( -integer => sub { 0 < $_ }, "How many attendees? " );
+my $abstract = prompt_long_text( 'Enter an abstract for the presentation:' );
+my $write_up = prompt_long_text( 'Enter a review of the meeting:' );
+chomp( $abstract, $write_up );
 
 die "Missing required parameter.\n"
-    unless defined $mon && defined $year && $author && $title && $attendees;
+    unless defined $mon && defined $year && $author && $title && $attendees && $abstract;
 
 # Identity transforms below to remove IO::Prompter special objects.
 my %vars = (
@@ -35,20 +42,22 @@ my %vars = (
     author => $author.'',
     title => $title.'',
     attendees => $attendees+0,
+    abstract => $abstract.'',
+    writeup => [ split /\n\n/, $write_up ],
 );
 
 add_talk_entry( \%vars );
 make_talk_dir( \%vars );
+add_feed_entry( \%vars );
 
 say <<EOM;
 
 Don't forget:
 
- * update the abstract in the talks.xml file.
- * fill in the rest of $vars{indexfile}.
- * update "What's New" on the index page.
- * remove entry from upcoming meetings page.
- * add to atom feed?
+ * fill in the rest of $vars{indexfile}
+ * add the presentation content
+ * update "What's New" on the index page
+ * remove entry from upcoming meetings page
 EOM
 
 exit;
@@ -108,3 +117,51 @@ sub make_talk_dir
     return;
 }
 
+sub add_feed_entry
+{
+    my ($vars) = @_;
+    my $entries = [];
+    my $file = 'atom_entries.json';
+    $entries = decode_json scalar read_file $file if -f $file;
+    unshift @{$entries}, {
+        title => qq[Notes for '$vars->{title}' posted.],
+        link => qq[http://houston.pm.org/talks/$vars->{year}talks/$vars->{yr}$vars->{mon}Talk/index.html],
+        id => qq[tag:houston.pm.org,2011-03:presentation:$vars->{yr}$vars->{mon}],
+        content => { type => 'xhtml', content => qq[<p>$vars->{abstract}</p>] },
+        published => strftime( '%Y-%m-%dT%H:%M:%SZ', gmtime ),
+        updated => strftime( '%Y-%m-%dT%H:%M:%SZ', gmtime ),
+        category => 'presentation',
+        category => 'technical meeting',
+    };
+    write_file( $file, encode_json $entries );
+
+    my $feed = XML::Atom::SimpleFeed->new(
+        id => qq[tag:houston.pm.org,2011-03:news],
+        title => qq[Houston.pm: What's New],
+        link => 'http://houston.pm.org/',
+        link => { rel => 'self', href => 'http://houston.pm.org/atom.xml' },
+        updated => strftime( '%Y-%m-%dT%H:%M:%SZ', gmtime ),
+        author => 'G. Wade Johnson',
+        category => 'news',
+    );
+
+    foreach my $entry ( @{$entries} )
+    {
+        $feed->add_entry( %{ $entry } );
+    }
+
+    write_file( 'atom.xml', $feed->as_string );
+    return;
+}
+
+sub prompt_long_text
+{
+    my ($prompt) = @_;
+    my $tmpfile = '.textarea.tmp';
+    write_file( $tmpfile, "# $prompt" );
+    system 'vim', '-i', 'NONE', $tmpfile;
+    my $output;
+    $output = read_file( $tmpfile ) if -s $tmpfile ne 2+length $prompt;
+    unlink $tmpfile;
+    return $output;
+}
